25b4e4240b8f2703a76f4bbb3bc9a4f0ea0c6f0a
SriSatyaPriya <srisatyapriya521@gmail.com>
2024-10-17 20:37:29 +0530

    Add files via upload

--- Multiplication.java
+++ Multiplication.java
@@ -5,9 +5,9 @@ public class Multiplication
         int i,j;
         for(i=1;i<=10;i++)
         {
-            for(j=1;j<=10;j++)
+            for(j=1;j<=5;j++)
             {
-                System.out.print(i +"x" +j + "=" +(i*j) + "\t\t");
+                System.out.print(j +"x" +i + "=" +(j*i) + "\t\t");
                  
             }
             System.out.println();
