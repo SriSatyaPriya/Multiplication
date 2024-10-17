286a1b954ab55d51b087e6a27d172635225cff68
SriSatyaPriya <srisatyapriya521@gmail.com>
2024-10-17 20:29:45 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,17 @@
+public class Multiplication
+{ 
+  public static void main(String[] args) 
+   {
+        int i,j;
+        for(i=1;i<=10;i++)
+        {
+            for(j=1;j<=10;j++)
+            {
+                System.out.print(i +"x" +j + "=" +(i*j) + "\t\t");
+                 
+            }
+            System.out.println();
+        }
+        
+    }
+}
